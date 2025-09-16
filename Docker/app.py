068e1d2ed2ada3d9961c397bc03c93b0f0c84551
mcarvalho2001 <manuelmariafc@gmail.com>
2025-09-16 11:14:56 +0100

    Docker not completed (would need to restart Windows)

--- Docker/app.py
+++ Docker/app.py
@@ -6,8 +6,7 @@ import socket
 app = Flask(__name__)
 redis = Redis(host=os.environ.get('REDIS_HOST', '127.0.0.1'), port=6379)
 
-
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return f"This webpage has been viewed {redis.get('hist').decode('utf-8')} times and hostname is {socket.gethostname()}.\n"
+    hits = redis.incr('hits')
+    return f"This webpage has been viewed {hits} times and hostname is {socket.gethostname()}.\n"
